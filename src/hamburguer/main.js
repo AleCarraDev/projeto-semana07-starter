28a85366136b4e4cb4564705045298a6eec6829a
AleCarraDev <22fimdetarde@gmail.com>
2022-02-13 17:07:24 -0300

    adicionando conteudo main

---
+++ src/hamburguer/main.js
@@ -0,0 +1,10 @@
+window.onload = (event) => {
+  event.preventDefault();
+  const links = document.querySelector(".navbar-links");
+
+  const btn = document.querySelector(".navbar-burger");
+
+  btn.addEventListener("click", function () {
+    links.classList.toggle("active");
+  });
+};
